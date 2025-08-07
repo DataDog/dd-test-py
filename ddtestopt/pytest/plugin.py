98df4f158c2fa3e0f07cc1f6936ea43b1c328c3d
Vítor De Araújo <vitor.dearaujo@datadoghq.com>
2025-08-07 21:21:39 +0000

    test status

--- ddtestopt/pytest/plugin.py
+++ ddtestopt/pytest/plugin.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 import re
+import typing as t
 
 import pytest
 
@@ -11,6 +12,7 @@ from ddtestopt.recorder import SessionManager
 from ddtestopt.recorder import SuiteRef
 from ddtestopt.recorder import TestRef
 from ddtestopt.recorder import TestSession
+from ddtestopt.recorder import TestStatus
 from ddtestopt.recorder import module_to_event
 from ddtestopt.recorder import session_to_event
 from ddtestopt.recorder import suite_to_event
@@ -38,9 +40,20 @@ def _get_module_path_from_item(item: pytest.Item) -> Path:
         return Path.cwd()
 
 
+class TestPhase:
+    SETUP = "setup"
+    CALL = "call"
+    TEARDOWN = "teardown"
+
+
+_ReportGroup = t.Dict[str, pytest.TestReport]
+
+
 class TestOptPlugin:
     def __init__(self):
         self.enable_ddtrace = True
+        self.reports_by_nodeid: t.Dict[str, _ReportGroup] = {}
+        self.excinfo_by_report: t.Dict[pytest.TestReport, pytest.ExceptionInfo] = {}
 
     def pytest_sessionstart(self, session: pytest.Session):
         self.session = TestSession(name="pytest")
@@ -75,6 +88,13 @@ class TestOptPlugin:
     def pytest_runtest_protocol(self, item, nextitem):
         test_ref = nodeid_to_test_ref(item.nodeid)
 
+        # test_module = self.session.get_child(
+        #     name=test_ref.suite.module.name,
+        #     or_create=lambda: TestModule(
+        #         ...
+        #     )
+        # )
+
         test_module, created = self.session.get_or_create_child(test_ref.suite.module.name)
         if created:
             test_module.set_attributes(module_path=_get_module_path_from_item(item))
@@ -93,6 +113,7 @@ class TestOptPlugin:
         with trace_context(self.enable_ddtrace) as context:
             yield
 
+        test.status = self._get_test_status(item.nodeid)
         test.finish()
 
         self.manager.writer.append_event(test_to_event(test, context))
@@ -104,6 +125,29 @@ class TestOptPlugin:
         if not next_test_ref or test_ref.suite.module != next_test_ref.suite.module:
             test_module.finish()
             self.manager.writer.append_event(module_to_event(test_module))
+
+    @pytest.hookimpl(hookwrapper=True)
+    def pytest_runtest_makereport(self, item: pytest.Item, call: pytest.CallInfo) -> None:
+        """
+        Save report and exception information for later use.
+        """
+        outcome = yield
+        report: pytest.TestReport = outcome.get_result()
+        self.reports_by_nodeid.setdefault(item.nodeid, {})[call.when] = report
+        self.excinfo_by_report[report] = call.excinfo
+
+    def _get_test_status(self, nodeid) -> TestStatus:
+        reports_dict = self.reports_by_nodeid.get(nodeid)
+        for phase in (TestPhase.SETUP, TestPhase.CALL, TestPhase.TEARDOWN):
+            report = reports_dict.get(phase)
+            if not report:
+                continue
+            if report.failed:
+                return TestStatus.FAIL
+            if report.skipped:
+                return TestStatus.SKIP
+
+        return TestStatus.PASS
 
 
 def pytest_configure(config):
