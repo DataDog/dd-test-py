963b7ee55444ade7af95f9903a97a852ef9b5ef4
Vítor De Araújo <vitor.dearaujo@datadoghq.com>
2025-08-07 21:47:47 +0000

    get test status from tests (#4)

--- ddtestopt/recorder.py
+++ ddtestopt/recorder.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from abc import ABC
+from collections import defaultdict
 from dataclasses import dataclass
 from enum import Enum
 import os
@@ -49,20 +50,45 @@ class TestItem(ABC):
         self.duration_ns: t.Optional[int] = None
         self.parent: t.Optional[TestItem] = None
         self.item_id = _gen_item_id()
-        self.status: TestStatus = TestStatus.FAIL
+        self.status: t.Optional[TestStatus] = None
         self.tags: t.Dict[str, str] = {}
         self.metrics: t.Dict[str, t.Union[int, float]] = {}
 
     def finish(self):
         self.duration_ns = time.time_ns() - self.start_ns
 
+    def get_status(self):
+        if not self.status:
+            self.status = self._get_status_from_children()
+        return self.status
+
+    def _get_status_from_children(self) -> TestStatus:
+        status_counts: t.Dict[TestStatus, int] = defaultdict(lambda: 0)
+        total_count = 0
+
+        for child in self.children.values():
+            status = child.get_status()
+            if status:
+                status_counts[status] += 1
+                total_count += 1
+
+        if status_counts[TestStatus.FAIL] > 0:
+            return TestStatus.FAIL
+
+        if status_counts[TestStatus.SKIP] == total_count:
+            return TestStatus.SKIP
+
+        return TestStatus.PASS
+
     def get_or_create_child(self, name):
         created = False
+
         if name not in self.children:
             created = True
             child = self.ChildClass(name=name)
             child.parent = self
             self.children[name] = child
+
         return self.children[name], created
 
 
@@ -190,7 +216,7 @@ def test_to_event(test: Test, context: TestContext) -> Event:
                 "test.module_path": test.parent.parent.module_path,
                 "test.name": test.name,
                 "test.skipped_by_itr": "false",
-                "test.status": "fail",
+                "test.status": test.get_status().value,
                 "test.suite": test.parent.name,
                 "test.type": "test",
                 "type": "test",
@@ -227,6 +253,7 @@ def suite_to_event(suite: TestSuite):
                 **suite.tags,
                 "span.kind": "test",
                 "test.suite": suite.name,
+                "test.status": suite.get_status().value,
                 "type": "test_suite_end",
             },
             "metrics": {
@@ -269,7 +296,7 @@ def module_to_event(module: TestModule):
                 "test.module": module.name,
                 "test.module_path": module.module_path,
                 "test.skipped_by_itr": "false",
-                "test.status": "fail",
+                "test.status": module.get_status().value,
                 "type": "test_module_end",
             },
             "metrics": {
@@ -308,7 +335,7 @@ def session_to_event(session: TestSession):
                 "test.itr.tests_skipping.type": "suite",
                 "test.itr.unskippable": "false",
                 "test.skipped_by_itr": "false",
-                "test.status": "fail",
+                "test.status": session.get_status().value,
                 "test.test_management.enabled": "true",
                 "type": "test_session_end",
             },
