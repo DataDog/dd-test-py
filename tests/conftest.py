31f07469be7046f4b2a694b796904559a0b6df91
Federico Mon <federico.mon@datadoghq.com>
2025-09-22 12:29:16 +0200

    test env var in CI

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,3 +2,11 @@
 
 # Enable pytester plugin for testing pytest plugins
 pytest_plugins = ["pytester"]
+
+import pytest
+import os
+
+
+@pytest.fixture(scope="session", autouse=True)
+def set_env():
+    os.environ["DD_API_KEY"] = "test-key"
