1f76134babdf15cde2511b2601eceea7ec52a2c5
Vítor De Araújo <vitor.dearaujo@datadoghq.com>
2025-09-24 13:16:14 +0000

    fix existing test

--- tests/internal/pytest/test_plugin.py
+++ tests/internal/pytest/test_plugin.py
@@ -673,6 +673,7 @@ class TestSessionLifecycleMethods:
         # Mock session
         mock_session = Mock()
         mock_session.exitstatus = pytest.ExitCode.OK
+        mock_session.config = Mock(workeroutput={})
 
         plugin.pytest_sessionfinish(mock_session)
 
